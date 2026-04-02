1e40bae355337e5e3f0655418f502c00aa9f400a
vickyrane9805 <162112327+vickyrane9805@users.noreply.github.com>
2026-04-02 09:18:31 +0530

    Update simple-intrest.sh

--- simple-intrest.sh
+++ simple-intrest.sh
@@ -1,1 +1,15 @@
 
+#!/bin/bash
+
+echo "Enter Principal:"
+read p
+
+echo "Enter Rate of Interest:"
+read r
+
+echo "Enter Time Period:"
+read t
+
+si=$(echo "scale=2; ($p * $r * $t) / 100" | bc)
+
+echo "Simple Interest is: $si"
